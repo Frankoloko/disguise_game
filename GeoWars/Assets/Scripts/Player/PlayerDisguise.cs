a9d8ecc9484766649179f7de79d477f428cfcba9
Francois <francoiskrugermail@gmail.com>
2021-08-02 19:07:05 +0200

    Added the distance check for disguise objets. Added the code to check all objects in scene with disguise object tags.

--- GeoWars/Assets/Scripts/Player/PlayerDisguise.cs
+++ GeoWars/Assets/Scripts/Player/PlayerDisguise.cs
@@ -4,8 +4,12 @@ using UnityEngine.InputSystem;
 public class PlayerDisguise : MonoBehaviour
 {
     public GameObject PlayerBody;
+    public float maxDistance = 5F;
+
     private Mesh PlayerOriginalMesh;
     private Vector3 PlayerOriginalScale;
+    private bool showLine = false;
+    private GameObject closestObject;
 
     void Start()
     {
@@ -14,8 +18,20 @@ public class PlayerDisguise : MonoBehaviour
         PlayerOriginalScale = PlayerBody.transform.localScale;
     }
 
+    void Update()
+    {
+        // Draw a line in debug mode to show which object is gettiing disguised to
+        if (Player.PlayerMovement.disableMovement)
+        {
+            Debug.DrawLine(PlayerBody.transform.position, closestObject.transform.position, Color.red);
+        }
+    }
+
+    // ------------------ PRIVATE FUNCTIONS ------------------
+
     private void OnDisguise(InputValue value)
     {
+        // JUST CHECK IF THE DISGUISE BUTTON WAS PRESSED OR RELEASED
         if (value.Get<float>() == 1.0)
         {
             PressDisguise();
@@ -28,42 +44,54 @@ public class PlayerDisguise : MonoBehaviour
 
     private void PressDisguise()
     {
-        Player.PlayerMovement.disableMovement = true;
-
-        // On PressDisguise, we transform the player's body into the nearest disguise object
+        // ON PressDisguise WE TRANSFORM THE PLAYER'S BODUY INTO THE NEAREST DISGUISE OBJECT
 
         // Get the closest object to disguise to
-        GameObject[] enemies = {GameObject.Find("Jerry"), GameObject.Find("Cylinder_01"), GameObject.Find("Sphere")};
-        GameObject closet_object = GetClosestObject(enemies);
+        GameObject[] objects = GameObject.FindGameObjectsWithTag("DisguiseObject");
+        closestObject = GetClosestObject(objects);
 
-        // Set the player's mesh to the same as the closest object
-        MeshFilter disguise_mesh_filter = closet_object.GetComponent<MeshFilter>();
-        PlayerBody.GetComponent<MeshFilter>().sharedMesh = disguise_mesh_filter.mesh;
+        // Check of the closest object is within the maxDistance value
+        float distance = Vector3.Distance(closestObject.transform.position, transform.position);
+        if (distance < maxDistance)
+        {
+            Player.PlayerMovement.disableMovement = true;
 
-        // Set the player's scale to the same as the closest object
-        PlayerBody.transform.localScale = closet_object.transform.localScale;
+            // Set the player's mesh to the same as the closest object
+            MeshFilter disguise_mesh_filter = closestObject.GetComponent<MeshFilter>();
+            PlayerBody.GetComponent<MeshFilter>().sharedMesh = disguise_mesh_filter.mesh;
 
-        // Uncommenting the below because it doesn't work like you'd expect. Regardless, I'm not sure if it will work at all since the objects might have different origins
-        // Set the player's Y-Position to the same as the closest object
-        // PlayerBody.transform.position = new Vector3(PlayerBody.transform.position.x, closet_object.transform.position.y, PlayerBody.transform.position.z);
+            // Set the player's scale to the same as the closest object
+            PlayerBody.transform.localScale = closestObject.transform.localScale;
+
+            // Uncommenting the below because it doesn't work like you'd expect. Regardless, I'm not sure if it will work at all since the objects might have different origins
+            // Set the player's Y-Position to the same as the closest object
+            // PlayerBody.transform.position = new Vector3(PlayerBody.transform.position.x, closestObject.transform.position.y, PlayerBody.transform.position.z);
+        } else {
+            Debug.Log($"Closest object distance ({distance}) is outside maxDistance value ({maxDistance})");
+        }
+
     }
 
     private void ReleaseDisguise()
     {
+        // ON ReleaseDisguise WE TRANSFORM THE PLAYER'S BODY BACK INTO THEIR ORIGINAL BODY
+
         Player.PlayerMovement.disableMovement = false;
 
-        // On ReleaseDisguise, we transform the player's body back into it's original body
         PlayerBody.GetComponent<MeshFilter>().sharedMesh = PlayerOriginalMesh;
         PlayerBody.transform.localScale = PlayerOriginalScale;
     }
 
-    GameObject GetClosestObject(GameObject[] enemies)
+    private GameObject GetClosestObject(GameObject[] objects)
     {
+        // If speed becomes an issue we can have a look at this script:
+        // https://www.youtube.com/watch?v=6rDlfYC4HxM
+
         GameObject bestTarget = null;
         float closestDistanceSqr = Mathf.Infinity;
         Vector3 currentPosition = transform.position;
 
-        foreach(GameObject potentialTarget in enemies)
+        foreach(GameObject potentialTarget in objects)
         {
             Vector3 directionToTarget = potentialTarget.transform.position - currentPosition;
             float dSqrToTarget = directionToTarget.sqrMagnitude;
